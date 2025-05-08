3daf7d197bb857e55f82457f558a1917daf0b7e1
Anh Kiet <phananhkiet1710@gmail.com>
2025-05-08 21:45:35 +0700

    cửa chính v1

--- lab2/script.js
+++ lab2/script.js
@@ -16,22 +16,12 @@ function blockRenderer(height) {
   };
 }
 
-const polygonRenderer = {
-  type: "simple",
-  symbol: {
-    type: "simple-fill",
-    color: "#D3D3D3",
-    outline: {
-      color: [211, 211, 211, 1],
-      width: 1,
-    },
-  },
-};
 require([
   "esri/Map",
   "esri/views/SceneView",
   "esri/layers/GeoJSONLayer",
   "esri/Graphic",
+  "esri/geometry/Mesh",
   "esri/geometry/Point",
   "esri/symbols/SimpleMarkerSymbol",
   "esri/layers/SceneLayer",
@@ -43,11 +33,19 @@ require([
   GeoJSONLayer,
   Graphic,
   Point,
+  Mesh,
   SimpleMarkerSymbol,
   SceneLayer,
   GraphicsLayer,
   esriRequest
 ) {
+  const sketchLayer = new GraphicsLayer({
+    elevationInfo: {
+      mode: "absolute-height",
+    },
+    title: "Sketched geometries",
+  });
+
   const map = new Map({
     basemap: "topo-vector",
     ground: "world-elevation",
@@ -79,7 +77,8 @@ require([
     },
     responseType: "json",
   };
-  // request json
+
+  // Thêm nền
   esriRequest("./data/cuaChinh/base.json", json_options).then(function (
     response
   ) {
@@ -99,16 +98,58 @@ require([
     });
   }
 
-  //Cửa chính
-  //Nửa bên trái
-  //Cổng trái
-  map.add(createGeoJsonLayer("cuaChinh/data", blockRenderer(4.5))); //O K' D1 S
-  map.add(createGeoJsonLayer("cuaChinh/data2", blockRenderer(1))); //W B1 N L
-  map.add(createGeoJsonLayer("cuaChinh/data3", blockRenderer(0.3))); //K' D1 N L
-  map.add(createGeoJsonLayer("cuaChinh/data4", blockRenderer(4.5))); //Q T N L
+  //START: Cửa chính
+  //Cổng trái, phải
+  map.add(createGeoJsonLayer("cuaChinh/data", blockRenderer(4.5))); //O K' D1 S, O' P C1 K
+  map.add(createGeoJsonLayer("cuaChinh/data2", blockRenderer(1))); //W B1 N L, L' M N1 W
+  map.add(createGeoJsonLayer("cuaChinh/data3", blockRenderer(0.3))); //K' D1 N L, L' M  C1 K
+  map.add(createGeoJsonLayer("cuaChinh/data4", blockRenderer(4.5))); //Q T N L, L' M A1 Q'
 
   //Cổng giữa
-  map.add(createGeoJsonLayer("cuaChinh/data5", blockRenderer(5.8))); //L N U R
-  map.add(createGeoJsonLayer("cuaChinh/data6", blockRenderer(1))); //J1 K1 U R
+  map.add(createGeoJsonLayer("cuaChinh/data5", blockRenderer(5.8))); //L N U R, R' Z M L'
+  map.add(createGeoJsonLayer("cuaChinh/data6", blockRenderer(1))); //J1 K1 U R, R' Z M1 L1
   map.add(createGeoJsonLayer("cuaChinh/data7", blockRenderer(0.3))); //L N M L'
+
+  //Thêm mesh
+  const mainGateL = new Point({
+    x: 106.6983156914999,
+    y: 10.7720521778461,
+    z: 20.2,
+  });
+
+  const mainGateR = new Point({
+    x: 106.80084488537197,
+    y: 10.867532728640802,
+    z: 20.2,
+  });
+
+  const mainGateM = new Point({
+    x: 106.80080197002785,
+    y: 10.86677410529397,
+    z: 20.2,
+  });
+
+  Mesh.createFromGLTF(mainGateM, "./meshObject/cuaChinh/cuaGiua.glb")
+    .then(function (geometry) {
+      geometry.scale(3, { origin: mainGateM });
+      geometry.rotate(0, 0, 203);
+      const graphic = new Graphic({
+        geometry,
+        symbol: {
+          type: "mesh-3d",
+          symbolLayers: [
+            {
+              type: "fill",
+              size: 10000,
+            },
+          ],
+        },
+      });
+      sketchLayer.add(graphic);
+    })
+    .catch(console.error);
+
+  map.add(sketchLayer);
+
+  //END: Cửa Chính
 });
